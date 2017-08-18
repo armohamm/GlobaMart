278b99335e95145c632474e5d4f84ba3378f3784
Kingfisher007 <sushant.mhetre@hotmail.com>
2017-08-18 14:51:20 +0000

    Adding handlebar templating library and top search bar

--- src/GlobaMartWeb/App_Start/BundleConfig.cs
+++ src/GlobaMartWeb/App_Start/BundleConfig.cs
@@ -14,6 +14,9 @@ namespace GlobaMartWeb
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
 
+            bundles.Add(new ScriptBundle("~/bundles/handlebar").Include(
+            "~/Scripts/handlebars-v4.0.10.js"));
+
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(

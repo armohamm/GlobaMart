48d5ccec1173d76b04668c262fe6118ac1f4028e
Sushant Mhetre <sushant.mhetre@hotmail.com>
2018-02-24 14:33:26 +0530

    merged about and contacts page

--- src/GlobaMartWeb/Controllers/HomeController.cs
+++ src/GlobaMartWeb/Controllers/HomeController.cs
@@ -19,12 +19,5 @@ namespace GlobaMartWeb.Controllers
 
             return View();
         }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
